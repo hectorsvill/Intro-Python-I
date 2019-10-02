a702c55888d57cce49ec0a8e78559de9b1328902
hectorsvill <hectorstevenvillasano@gmail.com>
2019-10-01 20:34:07 -0700

    14

--- src/14_cal.py
+++ src/14_cal.py
@@ -22,3 +22,16 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+yy = 2019
+
+mm = 0
+
+try: 
+  mm = int(input("Enter month: "))
+except ValueError:
+  mm = 10
+
+
+# print(mm)
+print(calendar.month(yy, mm))
+
